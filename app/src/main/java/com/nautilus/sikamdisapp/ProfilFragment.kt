afbe2fc51664f5e54fee3defdcf51aa639f061bd
jafar123 <js2185382@gmail.com>
2024-09-18 21:54:40 +0700

    UPDATE: MENAMBAHKAN LoginActivity

--- app/src/main/java/com/nautilus/sikamdisapp/ProfilFragment.kt
+++ app/src/main/java/com/nautilus/sikamdisapp/ProfilFragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ProfilFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfilFragment : Fragment() {
+class   ProfilFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
